2e0f40bac84fb33c63dc56e9328282ce9e5b43e5
Wendel <sswendel11@outlook.com>
2023-05-25 18:36:12 -0300

    teste vscode

--- src/main/java/sistema/projeto/Main.java
+++ src/main/java/sistema/projeto/Main.java
@@ -2,6 +2,6 @@ package sistema.projeto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("word world");
+        System.out.println("Wendel world");
     }
 }
