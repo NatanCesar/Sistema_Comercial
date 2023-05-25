89c68dc01e9d1381615a8964a79e2f3590277431
Wendel <sswendel11@outlook.com>
2023-05-25 18:14:57 -0300

    "psvm"

--- src/main/java/sistema/projeto/Main.java
+++ src/main/java/sistema/projeto/Main.java
@@ -2,6 +2,6 @@ package sistema.projeto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(" world");
     }
 }
